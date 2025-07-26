9c42a302dbdca8be51b1c8a5f6c0f6403d4a8c01
Veronica Martinez <vero_mtz_08@hotmail.com>
2025-07-25 19:55:36 -0600

    arreglo de navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,32 @@
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../stylesheets/Navbar.css';
 
 export default function Navbar() {
+  useEffect(() => {
+    const toggleButton = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('navbar-links');
+
+    const toggleMenu = () => {
+      navLinks.classList.toggle('active');
+    };
+
+    toggleButton.addEventListener('click', toggleMenu);
+
+    return () => {
+      toggleButton.removeEventListener('click', toggleMenu);
+    };
+  }, []);
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">Mi Portafolio</div>
-      <div className="navbar-links">
+
+      <button className="menu-toggle" id="menu-toggle">
+        &#9776;
+      </button>
+
+      <div className="navbar-links" id="navbar-links">
         <Link to="/">Inicio</Link>
         <Link to="/about">Sobre mí</Link>
         <Link to="/projects">Proyectos</Link>
